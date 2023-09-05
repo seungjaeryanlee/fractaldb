e0549ea00af7321a6edfe1471d283b17f1c36014
Ryan Lee <seungjaeryanlee@gmail.com>
2023-09-05 19:55:54 -0400

    Add factory method for CIFAR10

---
+++ datasets/__init__.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+import torchvision
+
+
+logger = logging.getLogger(__name__)
+DATA_ROOT = "data/"
+
+def create_dataset(config):
+    """Factory method for dataset."""
+    os.makedirs(DATA_ROOT, exist_ok=True)
+
+    if config.name == "torchvision.datasets.CIFAR10":
+        return torchvision.datasets.CIFAR10(DATA_ROOT, download=True)
+
+    logger.error(f"{config.name} is not a supported dataset.")
+    raise ValueError(f"{config.name} is not a supported dataset.")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from omegaconf import OmegaConf
 
+from datasets import create_dataset
 from models import create_model
 
 
@@ -7,3 +8,6 @@ if __name__ == "__main__":
     config = OmegaConf.load("configs/base.yaml")
     model = create_model(config.model)
     print(model)
+
+    dataset = create_dataset(config.dataset)
+    print(dataset)
