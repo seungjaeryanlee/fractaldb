70d6ca4c09968575de7c918c5864dd76cda3e7fc
Ryan Lee <seungjaeryanlee@gmail.com>
2023-09-07 22:31:19 -0400

    Use importlib to simplify transform factory method

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,6 +1,5 @@
+import importlib
 import logging
-
-import torchvision
 
 from utils import dict_without
 
@@ -8,15 +7,26 @@ logger = logging.getLogger(__name__)
 DATA_ROOT = "data/"
 
 
+def _get_class_from_str(module_class_str: str):
+    """Import and get Class from as string.
+
+    We assume config.name has format similar to module.submodule.Class
+    """
+    module_str, class_name = module_class_str.rsplit(".", 1)
+    Class = getattr(importlib.import_module(module_str), class_name)
+
+    return Class
+
+
 def create_transform(config):
     """Factory method for single transform."""
-    if config.name == "torchvision.transforms.CenterCrop":
-        return torchvision.transforms.CenterCrop(**dict_without(config, "name"))
-    if config.name == "torchvision.transforms.RandomVerticalFlip":
-        return torchvision.transforms.RandomVerticalFlip(**dict_without(config, "name"))
+    try:
+        Class = _get_class_from_str(config.name)
+    except:
+        logger.error(f"{config.name} is not a supported transform.")
+        raise ValueError(f"{config.name} is not a supported transform.")
 
-    logger.error(f"{config.name} is not a supported transform.")
-    raise ValueError(f"{config.name} is not a supported transform.")
+    return Class(**dict_without(config, "name"))
 
 
 def create_transforms(config):
